d6fb2755149fdc31bd62e3211df107cb02ba4924
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-06-09 21:54:58 +0200

    snack4

---
+++ snack4/main.js
@@ -0,0 +1,13 @@
+function creaTimer(tempo){
+    return function(){
+        setTimeout(() =>{
+            console.log("tempo scaduto!")
+        }, tempo)
+    }
+}
+const timer1 = creaTimer(1000)
+const timer2 = creaTimer(4000)
+
+timer1()
+timer2()
+
