75007c355954cafa0d0daf60e37b6af06c947cc1
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-06-09 19:10:30 +0200

    first commit

---
+++ snack1/main.js
@@ -0,0 +1,14 @@
+function somma1(x,y){
+    return x + y
+}
+
+const somma2 = function(x, y) {
+    return x + y;
+};
+
+const somma3 = (x,y) => x+y
+
+
+console.log(somma1(2,5))
+console.log(somma2(5,5))
+console.log(somma3(4,4))
